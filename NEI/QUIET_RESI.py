e2bfbed5b9505366c3f59e8d815f9387f0464adb
Rosenberg, Samuel (Sam) I <samuel.rosenberg@pnnl.gov>
2025-03-17 10:55:08 -0700

    sam's changes to QUIET_RESI

--- NEI/QUIET_RESI.py
+++ NEI/QUIET_RESI.py
@@ -56,69 +56,98 @@ df0['in.insulation_wall'].value_counts()
 
 
 
-#assign TL values based on spreadsheet, use placeholders for now to set up calcs
-df0['TL_win_125']  = 30  #to be dynamically assigned within the master dataframe based on the decided methodology 
-df0['TL_win_160']  = 30   
-df0['TL_win_200']  = 30   
-df0['TL_win_250']  = 30   
-df0['TL_win_315']  = 30   
-df0['TL_win_100']  = 30   
-df0['TL_win_400']  = 30   
-df0['TL_win_500']  = 30   
-df0['TL_win_630']  = 30   
-df0['TL_win_800']  = 30   
-df0['TL_win_1000'] = 30       
-df0['TL_win_1250'] = 30      
-df0['TL_win_1600'] = 30       
-df0['TL_win_2000'] = 30       
-df0['TL_win_2500'] = 30       
-df0['TL_win_3150'] = 30      
-df0['TL_win_4000'] = 30       
-df0['TL_win_5000'] = 30  
+# #assign TL values based on spreadsheet, use placeholders for now to set up calcs
+# df0['TL_win_125']  = 30  #to be dynamically assigned within the master dataframe based on the decided methodology 
+# df0['TL_win_160']  = 30   
+# df0['TL_win_200']  = 30   
+# df0['TL_win_250']  = 30   
+# df0['TL_win_315']  = 30   
+# df0['TL_win_100']  = 30   
+# df0['TL_win_400']  = 30   
+# df0['TL_win_500']  = 30   
+# df0['TL_win_630']  = 30   
+# df0['TL_win_800']  = 30   
+# df0['TL_win_1000'] = 30       
+# df0['TL_win_1250'] = 30      
+# df0['TL_win_1600'] = 30       
+# df0['TL_win_2000'] = 30       
+# df0['TL_win_2500'] = 30       
+# df0['TL_win_3150'] = 30      
+# df0['TL_win_4000'] = 30       
+# df0['TL_win_5000'] = 30  
 
 
-df0['TL_wal_125']  = 30   
-df0['TL_wal_160']  = 30   
-df0['TL_wal_200']  = 30   
-df0['TL_wal_250']  = 30   
-df0['TL_wal_315']  = 30   
-df0['TL_wal_100']  = 30   
-df0['TL_wal_400']  = 30   
-df0['TL_wal_500']  = 30   
-df0['TL_wal_630']  = 30   
-df0['TL_wal_800']  = 30   
-df0['TL_wal_1000'] = 30       
-df0['TL_wal_1250'] = 30      
-df0['TL_wal_1600'] = 30       
-df0['TL_wal_2000'] = 30       
-df0['TL_wal_2500'] = 30       
-df0['TL_wal_3150'] = 30      
-df0['TL_wal_4000'] = 30       
-df0['TL_wal_5000'] = 30   
+# df0['TL_wal_125']  = 30   
+# df0['TL_wal_160']  = 30   
+# df0['TL_wal_200']  = 30   
+# df0['TL_wal_250']  = 30   
+# df0['TL_wal_315']  = 30   
+# df0['TL_wal_100']  = 30   
+# df0['TL_wal_400']  = 30   
+# df0['TL_wal_500']  = 30   
+# df0['TL_wal_630']  = 30   
+# df0['TL_wal_800']  = 30   
+# df0['TL_wal_1000'] = 30       
+# df0['TL_wal_1250'] = 30      
+# df0['TL_wal_1600'] = 30       
+# df0['TL_wal_2000'] = 30       
+# df0['TL_wal_2500'] = 30       
+# df0['TL_wal_3150'] = 30      
+# df0['TL_wal_4000'] = 30       
+# df0['TL_wal_5000'] = 30   
 
-#calculate TL of whole assembly based on weighted average based on wwr
-#TL_tot = 10*log((A_win*10^(-TL_win/10)+A_wal*10^(-TL_wal/10))/A_tot)
+# #calculate TL of whole assembly based on weighted average based on wwr
+# #TL_tot = 10*log((A_win*10^(-TL_win/10)+A_wal*10^(-TL_wal/10))/A_tot)
 
-df0['TL_tot_100'] = 10*math.log10((df0['A_win']*10**(-df0['TL_win_100']/10)+df0['A_wal']*10**(-df0['TL_wal_100']/10))/df0['A_tot'])
-df0['TL_tot_125'] = 10*math.log10((df0['A_win']*10**(-df0['TL_win_125']/10)+df0['A_wal']*10**(-df0['TL_wal_125']/10))/df0['A_tot'])
-df0['TL_tot_160'] = 10*math.log10((df0['A_win']*10**(-df0['TL_win_160']/10)+df0['A_wal']*10**(-df0['TL_wal_160']/10))/df0['A_tot'])
-df0['TL_tot_200'] = 10*math.log10((df0['A_win']*10**(-df0['TL_win_200']/10)+df0['A_wal']*10**(-df0['TL_wal_200']/10))/df0['A_tot'])
-df0['TL_tot_250'] = 10*math.log10((df0['A_win']*10**(-df0['TL_win_250']/10)+df0['A_wal']*10**(-df0['TL_wal_250']/10))/df0['A_tot'])
-df0['TL_tot_315'] = 10*math.log10((df0['A_win']*10**(-df0['TL_win_315']/10)+df0['A_wal']*10**(-df0['TL_wal_315']/10))/df0['A_tot'])
-df0['TL_tot_400'] = 10*math.log10((df0['A_win']*10**(-df0['TL_win_400']/10)+df0['A_wal']*10**(-df0['TL_wal_400']/10))/df0['A_tot'])
-df0['TL_tot_500'] = 10*math.log10((df0['A_win']*10**(-df0['TL_win_500']/10)+df0['A_wal']*10**(-df0['TL_wal_500']/10))/df0['A_tot'])
-df0['TL_tot_630'] = 10*math.log10((df0['A_win']*10**(-df0['TL_win_630']/10)+df0['A_wal']*10**(-df0['TL_wal_630']/10))/df0['A_tot'])
-df0['TL_tot_800'] = 10*math.log10((df0['A_win']*10**(-df0['TL_win_800']/10)+df0['A_wal']*10**(-df0['TL_wal_800']/10))/df0['A_tot'])
-df0['TL_tot_1000']  = 10*math.log10((df0['A_win']*10**(-df0['TL_win_1000']/10)+df0['A_wal']*10**(-df0['TL_wal_1000']/10))/df0['A_tot'])
-df0['TL_tot_1250']  = 10*math.log10((df0['A_win']*10**(-df0['TL_win_1250']/10)+df0['A_wal']*10**(-df0['TL_wal_1250']/10))/df0['A_tot'])
-df0['TL_tot_1600']  = 10*math.log10((df0['A_win']*10**(-df0['TL_win_1600']/10)+df0['A_wal']*10**(-df0['TL_wal_1600']/10))/df0['A_tot'])
-df0['TL_tot_2000']  = 10*math.log10((df0['A_win']*10**(-df0['TL_win_2000']/10)+df0['A_wal']*10**(-df0['TL_wal_2000']/10))/df0['A_tot'])
-df0['TL_tot_2500']  = 10*math.log10((df0['A_win']*10**(-df0['TL_win_2500']/10)+df0['A_wal']*10**(-df0['TL_wal_2500']/10))/df0['A_tot'])
-df0['TL_tot_3150']  = 10*math.log10((df0['A_win']*10**(-df0['TL_win_3150']/10)+df0['A_wal']*10**(-df0['TL_wal_3150']/10))/df0['A_tot'])
-df0['TL_tot_4000']  = 10*math.log10((df0['A_win']*10**(-df0['TL_win_4000']/10)+df0['A_wal']*10**(-df0['TL_wal_4000']/10))/df0['A_tot'])
-df0['TL_tot_5000']  = 10*math.log10((df0['A_win']*10**(-df0['TL_win_5000']/10)+df0['A_wal']*10**(-df0['TL_wal_5000']/10))/df0['A_tot'])
+# df0['TL_tot_100'] = 10*math.log10((df0['A_win']*10**(-df0['TL_win_100']/10)+df0['A_wal']*10**(-df0['TL_wal_100']/10))/df0['A_tot'])
+# df0['TL_tot_125'] = 10*math.log10((df0['A_win']*10**(-df0['TL_win_125']/10)+df0['A_wal']*10**(-df0['TL_wal_125']/10))/df0['A_tot'])
+# df0['TL_tot_160'] = 10*math.log10((df0['A_win']*10**(-df0['TL_win_160']/10)+df0['A_wal']*10**(-df0['TL_wal_160']/10))/df0['A_tot'])
+# df0['TL_tot_200'] = 10*math.log10((df0['A_win']*10**(-df0['TL_win_200']/10)+df0['A_wal']*10**(-df0['TL_wal_200']/10))/df0['A_tot'])
+# df0['TL_tot_250'] = 10*math.log10((df0['A_win']*10**(-df0['TL_win_250']/10)+df0['A_wal']*10**(-df0['TL_wal_250']/10))/df0['A_tot'])
+# df0['TL_tot_315'] = 10*math.log10((df0['A_win']*10**(-df0['TL_win_315']/10)+df0['A_wal']*10**(-df0['TL_wal_315']/10))/df0['A_tot'])
+# df0['TL_tot_400'] = 10*math.log10((df0['A_win']*10**(-df0['TL_win_400']/10)+df0['A_wal']*10**(-df0['TL_wal_400']/10))/df0['A_tot'])
+# df0['TL_tot_500'] = 10*math.log10((df0['A_win']*10**(-df0['TL_win_500']/10)+df0['A_wal']*10**(-df0['TL_wal_500']/10))/df0['A_tot'])
+# df0['TL_tot_630'] = 10*math.log10((df0['A_win']*10**(-df0['TL_win_630']/10)+df0['A_wal']*10**(-df0['TL_wal_630']/10))/df0['A_tot'])
+# df0['TL_tot_800'] = 10*math.log10((df0['A_win']*10**(-df0['TL_win_800']/10)+df0['A_wal']*10**(-df0['TL_wal_800']/10))/df0['A_tot'])
+# df0['TL_tot_1000']  = 10*math.log10((df0['A_win']*10**(-df0['TL_win_1000']/10)+df0['A_wal']*10**(-df0['TL_wal_1000']/10))/df0['A_tot'])
+# df0['TL_tot_1250']  = 10*math.log10((df0['A_win']*10**(-df0['TL_win_1250']/10)+df0['A_wal']*10**(-df0['TL_wal_1250']/10))/df0['A_tot'])
+# df0['TL_tot_1600']  = 10*math.log10((df0['A_win']*10**(-df0['TL_win_1600']/10)+df0['A_wal']*10**(-df0['TL_wal_1600']/10))/df0['A_tot'])
+# df0['TL_tot_2000']  = 10*math.log10((df0['A_win']*10**(-df0['TL_win_2000']/10)+df0['A_wal']*10**(-df0['TL_wal_2000']/10))/df0['A_tot'])
+# df0['TL_tot_2500']  = 10*math.log10((df0['A_win']*10**(-df0['TL_win_2500']/10)+df0['A_wal']*10**(-df0['TL_wal_2500']/10))/df0['A_tot'])
+# df0['TL_tot_3150']  = 10*math.log10((df0['A_win']*10**(-df0['TL_win_3150']/10)+df0['A_wal']*10**(-df0['TL_wal_3150']/10))/df0['A_tot'])
+# df0['TL_tot_4000']  = 10*math.log10((df0['A_win']*10**(-df0['TL_win_4000']/10)+df0['A_wal']*10**(-df0['TL_wal_4000']/10))/df0['A_tot'])
+# df0['TL_tot_5000']  = 10*math.log10((df0['A_win']*10**(-df0['TL_win_5000']/10)+df0['A_wal']*10**(-df0['TL_wal_5000']/10))/df0['A_tot'])
 
-	
+# replacement for above
+# Assign TL values based on spreadsheet, using placeholders for now
+tl_frequencies = [
+    100, 125, 160, 200, 250, 315, 400, 500, 630, 800,
+    1000, 1250, 1600, 2000, 2500, 3150, 4000, 5000
+]
+
+# Set TL values for windows and walls
+for freq in tl_frequencies:
+    df0[f'TL_win_{freq}'] = 30
+    df0[f'TL_wal_{freq}'] = 30
+
+# Calculate TL of the whole assembly using weighted average
+for freq in tl_frequencies:
+    df0[f'TL_tot_{freq}'] = 10 * math.log10(
+        (df0['A_win'] * 10**(-df0[f'TL_win_{freq}'] / 10) +
+         df0['A_wal'] * 10**(-df0[f'TL_wal_{freq}'] / 10)) / df0['A_tot']
+    )
+
+# interpolate values in TL_database
+# Select only the relevant columns for interpolation
+cols_to_interpolate = [50, 63, 80, 100, 135, 250]
+df_subset = df_TL[cols_to_interpolate].T  # Transpose
+
+# Perform linear interpolation
+df_subset = df_subset.interpolate(method='linear', axis=0, limit_direction='both')
+
+# Transpose back to original format
+df_TL[cols_to_interpolate] = df_subset.T
+
 #in.geometry_wall_exterior_finish
 #in.geometry_wall_type
 #in.insulation_wall
